39178923ed8f89f0485e678b13c0b19abb116e86
Kasra Tabrizi <kasra@techwolf.be>
2022-02-10 11:09:50 +0100

    mobile

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const ipAPI =
   "https://geo.ipify.org/api/v2/country,city?apiKey=at_sk0QeXSyRd5htfsgJj8NxwHuHijia&ipAddress=213.118.163.190";
 
-let lat = 0;
-let lng = 0;
+let latValue = 0;
+let lngValue = 0;
 
 const ipAddress = document.getElementById("ip_address");
 const ipLocation = document.getElementById("ip_location");
 const timeZone = document.getElementById("time_zone");
 const isp = document.getElementById("isp");
+const input = document.getElementById("ip_input");
+const searchButton = document.getElementById("search_button");
+
+searchButton.addEventListener("click", () => {
+  console.log(input.value);
+});
 
 fetch(ipAPI)
   .then((res) => res.json())
@@ -15,8 +21,27 @@ fetch(ipAPI)
     console.log(data);
     ipAddress.textContent = data.ip;
     ipLocation.textContent = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`;
-    timeZone.textContent = data.location.timezone;
+    timeZone.textContent = "UTC " + data.location.timezone;
     isp.textContent = data.isp;
-    lat = data.location.lat;
-    lng = data.location.lng;
+    latValue = data.location.lat;
+    lngValue = data.location.lng;
+
+    var map = L.map("map", { zoomControl: false }).setView(
+      [latValue, lngValue],
+      15
+    );
+
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1Ijoia2FzcmF0YWJyaXppIiwiYSI6ImNremZnMnN1YjJzZW8yb245YmQ1NXQwajgifQ.5WusdASChE1YPhQyXj3P8Q",
+      }
+    ).addTo(map);
   });
